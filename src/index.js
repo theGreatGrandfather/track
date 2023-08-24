f6b06f4db50ed193fac9f94e698ec985681181aa
Praded3 <pradid.store@gmail.com>
2023-08-24 12:45:23 +0300

    12

---
+++ src/index.js
@@ -0,0 +1,37 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const modalForm = document.querySelector('.truckload__form');
+    const TOKEN = '6407921969:AAGnKf8qyUQ3H2x0SNI61kLnO0qEHU1R2Ik';
+    const CHAT_ID = '-1001903507381';
+    const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
+    
+    modalForm.addEventListener('submit', formSend);
+
+    function sendMessage(formData) {
+        let messageToTg = `<b>New small action</b>\n`;
+
+        for (let entry of formData.entries()) {
+            if (entry[1] !== '') {
+                messageToTg += `${entry[0]} : ${entry[1]}\n`;
+            }
+        }
+        return messageToTg;
+    }
+
+    async function formSend(e) {
+        e.preventDefault();
+        let formData = new FormData(modalForm);
+        
+        const messageToTg = sendMessage(formData);
+
+        try {
+            await axios.post(URI_API, {
+                chat_id: CHAT_ID,
+                parse_mode: 'html',
+                text: messageToTg,
+            });
+            console.log('Message sent to Telegram successfully.');
+        } catch (error) {
+            console.error('Error sending message to Telegram:', error);
+        }
+    }
+});
